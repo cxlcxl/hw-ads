abaeebef66c7c96908a397898b85175537e73a54
xiaoliang.cheng <xiaoliang.cheng@idreamsky.com>
2023-02-24 13:45:39 +0800

    fix: 应用调度

--- cronJobs/jobs/app/app.go
+++ cronJobs/jobs/app/app.go
@@ -14,9 +14,10 @@ func ReportApp() {
 	hlog.NewLog(logrus.InfoLevel, "jobs-app").Log(logrus.Fields{}, "App job start")
 	defer func() {
 		_ = model.NewJob(vars.DBMysql).UpdateLastSchedule(vars.ApiModuleApp)
+		hlog.NewLog(logrus.InfoLevel, "jobs-app").Log(logrus.Fields{}, "App job end")
 	}()
 	if err := logic.NewAppQueryLogic().AppQuery(); err != nil {
-		hlog.NewLog(logrus.WarnLevel, "jobs-app-schedule").Log(logrus.Fields{}, err)
+		hlog.NewLog(logrus.ErrorLevel, "jobs-app-schedule").Log(logrus.Fields{}, err)
 		return
 	}
 	if err := model.NewJob(vars.DBMysql).UpdateJobDayByModule(vars.ApiModuleApp, time.Now().Format(vars.DateFormat)); err != nil {
@@ -24,7 +25,6 @@ func ReportApp() {
 		return
 	}
 
-	hlog.NewLog(logrus.InfoLevel, "jobs-app").Log(logrus.Fields{}, "App job end")
 	fmt.Println()
 	fmt.Println()
 }
